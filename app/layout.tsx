45507496fe3a0cbd4b11109ae0715c6747db67c1
Kartikeye Gupta <kg330@duke.edu>
2024-11-05 02:16:55 -0500

    test logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,11 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
+                    <Link href="/">
+                      <img src="/Users/kartikeye/cubenme/app/cube.png" alt="Logo" style={{ height: '50px', cursor: 'pointer' }} />
+                    </Link>
                     <Link href={"/"}>The Cube LLC</Link>
                     <div className="flex items-center gap-2">
-                      <DeployButton />
                     </div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
