e4cec8ef01c711bdf7a715a1360d7497cc867d8c
Kartikeye (Tiki) Gupta <kg330@duke.edu>
2024-12-02 01:44:49 -0500

    resolve

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -5,7 +5,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { Select as CNSelect, SelectItem, SelectTrigger, SelectValue,  SelectContent, SelectLabel} from "@/components/ui/select";
-import { SmtpMessage } from "../smtp-message";
 
 export default async function Signup(props: {
   searchParams: Promise<Message>;
